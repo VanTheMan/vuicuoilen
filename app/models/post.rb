0cc200ca80c42f0bfc5a6741cd19f427e9d1a6ed
Toan Nguyen <toannm3110@gmail.com>
2012-09-16 14:46:53 +0700

    add footer + header to layout

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,8 @@ class Post
 	belongs_to :user
 	belongs_to :category, inverse_of: nil
 	has_and_belongs_to_many :favourite_users, class_name: "User", inverse_of: :favourite_posts
-	
+
 	#attributes
 	field :content, type: String
+	field :voted_user_ids, type: Array, default: []
 end
